3acd90ba8d60336ef6843c6e3ae15ba7247cdc34
synthmax <max@koblo.com>
2011-06-27 11:45:53 +0200

    styling of error mesages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :role
   has_secure_password
   validates_presence_of :password, :on => :create  
+  validates_presence_of :name
   validates_presence_of :email  
   validates_uniqueness_of :email
 
