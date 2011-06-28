fc8771cc3d9becbaf9d48fb28090fcc26c410bb8
synthmax <max@koblo.com>
2011-06-28 17:06:49 +0200

    pagination and search on text_content

--- app/models/text_content.rb
+++ app/models/text_content.rb
@@ -1,2 +1,12 @@
 class TextContent < ActiveRecord::Base
+  
+  
+  def self.search(search)
+    if search
+      where('title LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
+  
 end
