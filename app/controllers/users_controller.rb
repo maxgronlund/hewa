83994c2326b7e27269f4539a325bfd6dcf720445
synthmax <max@koblo.com>
2011-07-03 00:00:07 +0200

    changer crop_update so it works on all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < InheritedResources::Base
 
   
   def index
-    return_path users_path # !!! same as line 6?
+    return_path users_path # !!! same as line 10?
 #    @is_first_user = User.first.id == 1
     session[:go_to_after_edit] = users_path
     @users = User.search(params[:search]).order(sort_column + " " + sort_direction).page(params[:page]).per(25)
@@ -38,7 +38,8 @@ class UsersController < InheritedResources::Base
   end
 
   def crop_update
-    @user = current_user
+  #  @user = current_user                        #!!! current user? no no no 
+    @user = User.find(params[:id])
     @user.crop_x = params[:user]["crop_x"]
     @user.crop_y = params[:user]["crop_y"]
     @user.crop_h = params[:user]["crop_h"]
