c01036186540e967b8b7bff5aa38d0abe8547697
Lars Hoeg <larshoeg@gmail.com>
2011-07-14 15:31:53 +0200

    misc test cleaup

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,5 +3,19 @@ require 'factory_girl'
 Factory.define :user do |u|
   u.name 'Test User'
   u.email 'user@test.com'
-  u.password 'please'
+  u.password 'secret'
+end
+
+Factory.define :admin_user do |u|
+  u.name 'Test Admin User'
+  u.email 'admin@test.com'
+  u.password 'secret'
+  u.role 'admin'
+end
+
+Factory.define :super_user do |u|
+  u.name 'Test Super User'
+  u.email 'super@test.com'
+  u.password 'secret'
+  u.role 'super'
 end
