9e868e994fad4510244467691e67f55b7a7a54ad
synthmax <max@koblo.com>
2011-07-27 23:11:04 +0200

    falback image added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @menu = 'home'
     session[:go_to_after_edit] = root_path
-    @product_lines = ProductLine.promote_on_front_page
+    @promoted_product_lines = ProductLine.promote_on_front_page
     @news_blogs = NewsBlog.all
   end
 
