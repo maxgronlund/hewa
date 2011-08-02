2102ffa7218f9bf65752054d6e30c17dc789fe5a
synthmax <max@koblo.com>
2011-08-02 11:40:06 +0200

    -

--- app/models/text_content.rb
+++ app/models/text_content.rb
@@ -1,6 +1,6 @@
 class TextContent < ActiveRecord::Base
   validates :identity, :presence => true, :uniqueness => true
-  validates_presence_of :title
+  validates_presence_of :title, :identity
   
   def self.search(search)
     if search
