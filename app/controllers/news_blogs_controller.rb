a84210c4bf72db2940e3deb9e2d93090f76db823
Lars Hoeg <larshoeg@gmail.com>
2011-08-02 10:54:59 +0200

    newsletters with pdf

--- app/controllers/news_blogs_controller.rb
+++ app/controllers/news_blogs_controller.rb
@@ -1,2 +1,16 @@
 class NewsBlogsController < InheritedResources::Base
+
+  def index
+    @menu = 'news_blogs'
+    index!
+  end
+
+  def create
+    create! { news_blogs_path }
+  end
+
+  def update
+    update! { news_blogs_path }
+  end
+
 end
--- app/models/news_blog.rb
+++ app/models/news_blog.rb
@@ -1,2 +1,8 @@
 class NewsBlog < ActiveRecord::Base
+  default_scope order('created_at DESC')
+
+  mount_uploader :newsletter, NewsletterUploader
+
+  validate :title, :presence => true
+  validate :newsletter, :presence => true
 end
