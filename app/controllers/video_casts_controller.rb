2de97beb7fca145021661c5a6ef685081c2d2bc5
synthmax <max@koblo.com>
2011-08-16 18:09:49 +0200

    working on internalizasation

--- app/controllers/video_casts_controller.rb
+++ app/controllers/video_casts_controller.rb
@@ -1,2 +1,18 @@
 class VideoCastsController < InheritedResources::Base
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @menu = 'admin'
+    session[:go_to_after_edit] = video_casts_path
+    @video_casts = VideoCast.search(params[:search]).order(sort_column + " " + sort_direction).page(params[:page]).per(25)
+  end
+
+private  
+  def sort_column  
+    TextContent.column_names.include?(params[:sort]) ? params[:sort] : "title"  
+  end 
+
+  def sort_direction  
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"  
+  end
 end
--- app/models/video_cast.rb
+++ app/models/video_cast.rb
@@ -1,2 +1,19 @@
 class VideoCast < ActiveRecord::Base
+  
+  
+  
+  def self.search(search)
+    if search
+      where('title LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
+  
+  
+  
+  def self.with_identity(identity)
+    where(:identity => identity).first
+  end
+  
 end
