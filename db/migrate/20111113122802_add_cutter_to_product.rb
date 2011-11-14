a34b902895e9ec9877a7e228f59135437da00d88
Synthmax <max@synthmax.dk>
2011-11-14 14:11:09 +0100

    nothing commited

--- db/migrate/20111113122802_add_cutter_to_product.rb
+++ db/migrate/20111113122802_add_cutter_to_product.rb
@@ -3,25 +3,14 @@ class AddCutterToProduct < ActiveRecord::Migration
    # remove_column :products, :cutter
     add_column :products, :cutter, :boolean
     
-    ProductLine.all.each do |product_line|
-      if product_line.id == 40
-        product_line.cutter = true
-      else
-        product_line.cutter = false
-      end
-      product_line.save
-    end
-    
-    
     Product.all.each do |product|
-      if product.product_line_id == 40
+      if product.product_line_id == 4
         product.cutter = true
       else
         product.cutter = false
       end
       product.save
     end
-    
   end
   def down
     remove_column :products, :cutter
